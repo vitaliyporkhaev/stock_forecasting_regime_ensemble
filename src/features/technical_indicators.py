73b7f93474dbd5f02d096804e09261171ab6f855
Vitaliy Porkhaev <v1flit@mail.ru>
2026-05-13 00:35:51 +0300

    fix

--- src/features/technical_indicators.py
+++ src/features/technical_indicators.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
 
-# def rsi(series: pd.Series, period: int = 14):
-#     delta = series.diff()
+def rsi(series: pd.Series, period: int = 14):
+    delta = series.diff()
 
-#     gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
-#     loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
+    gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
 
-#     rs = gain / (loss + 1e-9)
-#     return 100 - (100 / (1 + rs))
+    rs = gain / (loss + 1e-9)
+    return 100 - (100 / (1 + rs))
+
 
 def add_rsi(df, period=14):
     delta = df["close"].diff()
